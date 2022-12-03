8e9bc406ed7d1bc13441407be4e2dc4da689d812
jlewisit <joelewis.mortgage@gmail.com>
2022-12-03 14:06:41 -0800

    added apollo to app.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+
+// establish apollo client
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
 
 function App() {
   return (
