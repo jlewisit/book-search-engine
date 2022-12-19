e65670ea1bab1406fca1d6db8b210e9420dc58bd
jlewisit <joelewis.mortgage@gmail.com>
2022-12-19 09:27:20 -0800

    SavedBooks.js updated with graphql token

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 // import { getMe, deleteBook } from '../utils/API';
+import gql from "graphql-tag";
 import { REMOVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
@@ -21,7 +22,7 @@ const SavedBooks = () => {
           return false;
         }
 
-        const response = await getMe(token);
+        const response = await gql(token);
 
         if (!response.ok) {
           throw new Error('something went wrong!');
